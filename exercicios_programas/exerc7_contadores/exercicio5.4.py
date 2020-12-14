134c70077c6f588047441901fc8a58d2a4fc4de7
Robinson <robinpaulista85@gmail.com>
2020-12-14 14:34:25 -0300

    Novas inserções

---
+++ exercicios_programas/exerc7_contadores/exercicio5.4.py
@@ -0,0 +1,2 @@
+''' 5.4 Modifique o programa anterior anterior para imprimir de 1 até o número digitado 
+pelo usuário, mas, dessa vez, apenas os números ímpares. '''
---
+++ exercicios_programas/exerc7_contadores/exercicio5.5.py
@@ -0,0 +1,4 @@
+''' 5.5 Reescreva o programa anterior (5.4) para escrever os 10 primeiros múltiplos de 3.
+
+(5.4 Modifique o programa anterior anterior para imprimir de 1 até o número digitado 
+pelo usuário, mas, dessa vez, apenas os números ímpares.) '''
