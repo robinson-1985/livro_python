7641748cdbf8e69ae93f7f49c64afbe15f738e46
Robinson <robinpaulista85@gmail.com>
2020-12-15 18:14:45 -0300

    Novas inserções

---
+++ comments/exemplo5.2.1.py
@@ -0,0 +1,9 @@
+# Exemplo de cálculo de média com acumulador:
+
+x = 1
+soma = 0
+while x <= 5:
+    n = int(input(f"{x} Digite o número: "))
+    soma = soma + n
+    x = x + 1
+print(f"Média: {soma/ 5:5.2f}")
