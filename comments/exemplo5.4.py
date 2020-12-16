64e2026a2ffe29db0d4c2dac2b9300c0ad969b11
Robinson <robinpaulista85@gmail.com>
2020-12-16 16:17:58 -0300

    Novas inserções

---
+++ comments/exemplo5.4.py
@@ -0,0 +1,9 @@
+# tabuada
+
+tabuada = 1
+while tabuada <= 10:
+    número = 1
+    while número <= 10:
+        print(f"{tabuada} x {número} = {tabuada * número}")
+        número += 1
+    tabuada += 1
---
+++ exercicios_programas/exerc10_repeticoes_aninhadas/exercicio5.27.py
@@ -0,0 +1,3 @@
+''' 5.27 Escreva um programa que veriﬁque se um número é palíndromo. Um número é 
+palíndromo se continua o mesmo caso seus dígitos sejam inver tidos. 
+Exemplos:  454, 10501'''
