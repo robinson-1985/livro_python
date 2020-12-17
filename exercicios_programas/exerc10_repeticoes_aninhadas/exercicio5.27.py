6e49e9ae26127d0f5f3f04000edf7815b65bc53d
Robinson <robinpaulista85@gmail.com>
2020-12-17 15:10:04 -0300

    Lista de exercícios

--- exercicios_programas/exerc10_repeticoes_aninhadas/exercicio5.27.py
+++ exercicios_programas/exerc10_repeticoes_aninhadas/exercicio5.27.py
@@ -1,3 +1,2 @@
 ''' 5.27 Escreva um programa que veriﬁque se um número é palíndromo. Um número é 
-palíndromo se continua o mesmo caso seus dígitos sejam inver tidos. 
-Exemplos:  454, 10501'''
+palíndromo se continua o mesmo caso seus dígitos sejam invertidos. Exemplos: 454, 10501. '''
